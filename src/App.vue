8c38ca8cdbd165aaeb24795d57068c5745914360
aohan <zhangaohan0905@163.com>
2023-10-30 22:03:58 +0800

    搭建项目框架, 引入element-plus

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const goToList = () => {
+  router.push('/list')
+}
+</script>
+
+<template>
+  <div>我是App</div>
+  <el-button @click="$router.push('/home')">主页</el-button>
+  <el-button @click="goToList">列表</el-button>
+</template>
+
+<style scoped></style>
